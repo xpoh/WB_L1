d02d03bac0302b7659aca3515c739a903a9ab179
laddr@mail.ru <SE8sQGgNpjUMuQy>
2022-06-21 14:47:55 +0300

    task 6

---
+++ 6.go
@@ -0,0 +1,73 @@
+/*
+	Реализовать все возможные способы остановки выполнения горутины.
+*/
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"sync"
+	"time"
+)
+
+func main() {
+	wg := sync.WaitGroup{} // waitgroup for wait for all work done
+
+	// Stop by cancel func:
+	ctx, cancel := context.WithCancel(context.Background())
+	wg.Add(1)   // inc for one worker
+	go func() { // run worker
+		defer wg.Done()
+		<-ctx.Done() // check for job done
+		fmt.Println("Exit bu func cancel")
+		return
+	}()
+	time.Sleep(time.Second)
+	cancel()
+
+	// Stop by Timeout:
+	ctxt, _ := context.WithTimeout(context.Background(), time.Second) // context for workers with timeout
+	wg.Add(1)                                                         // inc for one worker
+	go func() {                                                       // run worker
+		defer wg.Done()
+		<-ctxt.Done() // check for job done
+		fmt.Println("Exit by timeout")
+		return
+	}()
+
+	// Stop by deadline:
+	ctxd, _ := context.WithDeadline(context.Background(), time.Now().Add(5*time.Second)) // context for workers with deadline
+	wg.Add(1)                                                                            // inc for one worker
+	go func() {                                                                          // run worker
+		defer wg.Done()
+		<-ctxd.Done() // check for job done
+		fmt.Println("Exit by deadline")
+		return
+	}()
+
+	// Stop by signal notify:
+	ch := make(chan os.Signal, 1)
+	fmt.Println("Press ctrl+c")
+	signal.Notify(ch, os.Interrupt)
+	wg.Add(1)   // inc for one worker
+	go func() { // run worker
+		defer wg.Done()
+		<-ch // check for job done
+		fmt.Println("Exit by SIG_INT")
+		return
+	}()
+
+	wg.Wait()
+
+	// Interrupt by stop main func:
+	go func() { // run worker
+		time.Sleep(time.Hour)
+	}()
+
+	// Interrupt self then job done
+	go func() { // run worker
+		// job done
+	}()
+}
