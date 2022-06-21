c308bde70ef71513199bd134b0eea2c4e8524f32
laddr@mail.ru <SE8sQGgNpjUMuQy>
2022-06-21 18:21:07 +0300

    task 14

---
+++ 15.go
@@ -0,0 +1,26 @@
+/*
+	15. К каким негативным последствиям может привести данный фрагмент кода, и как
+	это исправить? Приведите корректный пример реализации.
+
+	var justString string
+
+	func someFunc() {
+		v := createHugeString(1 << 10)
+		justString = v[:100]
+	}
+	func main() {
+		someFunc()
+	}
+	Проблема, что выделяется большой объем памяти, который не используется
+	justString = v[:100] - здесь все равно выделяется новая память
+*/
+package main
+
+var justString string
+
+func someFunc() {
+	justString = createHugeString(100)
+}
+func main() {
+	someFunc()
+}
